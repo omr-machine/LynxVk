ad31d8bb72237e73e0177313f1e4ddd0b01af289
omr-machine <40251498+omr-machine@users.noreply.github.com>
2025-11-15 00:18:42 +0300

    Delete src directory

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use simplelog::SimpleLogger;
-
-mod mess;
-
-fn main() {
-    let _ = SimpleLogger::init(simplelog::LevelFilter::Info, simplelog::Config::default());
-    mess::ash_test_main::ash_test_main();
-}
